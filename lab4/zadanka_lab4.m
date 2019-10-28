77b2ff17b22879fd11172b932560112740f4424a
Dawid Tobor <dawidtobor@op.pl>
2019-10-28 16:23:41 +0100

    Zadanie 3

--- lab4/zadanka_lab4.m
+++ lab4/zadanka_lab4.m
@@ -1,6 +1,6 @@
 %%
 % Zadanie 1
-%%
+%
 
 function [test_out_value, critical_value, decision] = ...
     sig_level_avg(sig_level, X, avg_tested, type_of_test, plot_result)
@@ -51,7 +51,7 @@ end
 
 %%
 % Zadanie 2
-%%
+%
 
 function [test_out_value, critical_value, decision] = ...
     sig_level2_avg(sig_level, X1, X2, type_of_test, plot_result)
@@ -85,6 +85,55 @@ switch(type_of_test)
     case 'bilateral'
         critical_value = norminv(1-(sig_level/2), 0, 1);
         critical_value_neg = -norminv(1-(sig_level/2), 0, 1);
+        if(test_out_value <= critical_value_neg || test_out_value >= critical_value)
+            decision = false;
+        else
+            decision = true;
+        end
+    otherwise
+        disp('Wrong method has been chosed!');
+        test_out_value = 0;
+        critical_value = 0;
+        decision = false;
+        return
+end
+end
+
+
+%%
+% Zadanie 3
+%
+
+function [test_out_value, critical_value, decision] = ...
+    sig_level_var(sig_level, X, var_tested, type_of_test, plot_result)
+
+if ~exist('plot_result', 'var')
+    plot_result = false;
+end
+
+sigma = std(X);
+[n, ~] = size(X);
+
+test_out_value = (n * sigma^2) / var_tested; % chi-square
+
+switch(type_of_test)
+    case 'left sided'
+        critical_value = -chi2inv(1-sig_level, n-1);
+        if(test_out_value <= critical_value)
+            decision = false;
+        else
+            decision = true;
+        end
+    case 'right sided'
+        critical_value = chi2inv(1-sig_level, n-1);
+        if(test_out_value >= critical_value)
+            decision = false;
+        else
+            decision = true;
+        end
+    case 'bilateral'
+        critical_value = chi2inv(1-(sig_level/2), n-1);
+        critical_value_neg = -chi2inv(1-(sig_level/2), n-1);
         if(test_out_value <= critical_value_neg || test_out_value >= critical_value)
             decision = false;
         else
